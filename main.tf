ebd0fe8f9ba58cbd64214cbbc8882abab9fdf29e
pcsprashanth <pathaneniprashanth@yahoo.in>
2024-04-05 17:55:23 +0530

    now

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ data "azurerm_client_config" "core" {}
 
 module "enterprise_scale" {
   source  = "Azure/caf-enterprise-scale/azurerm"
-  version = "4.0.0"
+  version = "5.0.0"
 
   default_location = var.default_location
 
