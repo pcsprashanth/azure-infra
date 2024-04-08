1d2f03b84ebf175d1566479f680bd1a4b53f0524
pcsprashanth <pathaneniprashanth@yahoo.in>
2024-04-08 19:38:26 +0530

    MG's

--- main.tf
+++ main.tf
@@ -27,120 +27,120 @@ module "enterprise_scale" {
   # The following configuration is used to deploy the management group structure
   ###### Deploys MG structure with naming convention provided by customer and disables deployment of default core structure ######
 
-  deploy_core_landing_zones = true
+  deploy_core_landing_zones = false
 
-  # custom_landing_zones = {
-  #     "${var.root_id}" = {
-  #       display_name               = "${lower(var.root_name)}"
-  #       parent_management_group_id = "${data.azurerm_client_config.core.tenant_id}"
-  #       subscription_ids           = []
-  #       archetype_config = {
-  #         archetype_id   = "default_empty"
-  #         parameters     = {}
-  #         access_control = {}
-  #       }
-  #     }
-  #      "${var.root_id}-platform" = {
-  #       display_name               = "Platform"
-  #       parent_management_group_id = "${var.root_id}"
-  #       subscription_ids           = []
-  #       archetype_config = {
-  #         archetype_id   = "default_empty"
-  #         parameters     = {}
-  #         access_control = {}
-  #       }
-  #     }
-  #     "${var.root_id}-landingzones" = {
-  #       display_name               = "Landing Zones"
-  #       parent_management_group_id = "${var.root_id}"
-  #       subscription_ids           = []
-  #       archetype_config = {
-  #         archetype_id   = "default_empty"
-  #         parameters     = {}
-  #         access_control = {}
-  #       }
-  #     }
-  #     "${var.root_id}-decom" = {
-  #       display_name               = "Decommissioned"
-  #       parent_management_group_id = "${var.root_id}"
-  #       subscription_ids           = []
-  #       archetype_config = {
-  #         archetype_id   = "default_empty"
-  #         parameters     = {}
-  #         access_control = {}
-  #       }
-  #       # depends_on= [azurerm_subscription.connectivity1,azurerm_subscription.connectivity2]
-  #     }
-  #     "${var.root_id}-sandbox" = {
-  #       display_name               = "Sandbox"
-  #       parent_management_group_id = "${var.root_id}"
-  #       subscription_ids           = []
-  #       archetype_config = {
-  #         archetype_id   = "default_empty"
-  #         parameters     = {}
-  #         access_control = {}
-  #       }
-  #     }
-  #     "${var.root_id}-iam" = {
-  #       display_name               = "Identity and Access Managment"
-  #       parent_management_group_id = "${var.root_id}-platform"
-  #       subscription_ids           = []
-  #       archetype_config = {
-  #         archetype_id   = "default_empty"
-  #         parameters     = {}
-  #         access_control = {}
-  #       }
-  #     }
-  #     ##### landing zone #####
-  #     "${var.root_id}-management" = {
-  #       display_name               = "Management"
-  #       parent_management_group_id = "${var.root_id}-platform"
-  #       subscription_ids           = []
-  #       archetype_config = {
-  #         archetype_id   = "default_empty"
-  #         parameters     = {}
-  #         access_control = {}
-  #       }
-  #     }
-  #     "${var.root_id}-connectivity" = {
-  #       display_name               = "Connectivity"
-  #       parent_management_group_id = "${var.root_id}-platform"
-  #       subscription_ids           = []
-  #       archetype_config = {
-  #         archetype_id   = "default_empty"
-  #         parameters     = {}
-  #         access_control = {}
-  #       }
-  #     }
-  #     "${var.root_id}-uat" = {
-  #       display_name               = "UAT"
-  #       parent_management_group_id = "${var.root_id}-landingzones"
-  #       subscription_ids           = []
-  #       archetype_config = {
-  #         archetype_id   = "default_empty"
-  #         parameters     = {}
-  #         access_control = {}
-  #       }
-  #     }
-  #     "${var.root_id}-dev" = {
-  #       display_name               = "Dev"
-  #       parent_management_group_id = "${var.root_id}-landingzones"
-  #       subscription_ids           = []
-  #       archetype_config = {
-  #         archetype_id   = "default_empty"
-  #         parameters     = {}
-  #         access_control = {}
-  #       }
-  #     }
-  #     "${var.root_id}-prod" = {
-  #       display_name               = "Prod"
-  #       parent_management_group_id = "${var.root_id}-landingzones"
-  #       subscription_ids           = []
-  #       archetype_config = {
-  #         archetype_id   = "default_empty"
-  #         parameters     = {}
-  #         access_control = {}
-  #       }
-  #     }
-  # }
+  custom_landing_zones = {
+      "${var.root_id}" = {
+        display_name               = "${lower(var.root_name)}"
+        parent_management_group_id = "${data.azurerm_client_config.core.tenant_id}"
+        subscription_ids           = []
+        archetype_config = {
+          archetype_id   = "default_empty"
+          parameters     = {}
+          access_control = {}
+        }
+      }
+       "${var.root_id}-platform" = {
+        display_name               = "Platform"
+        parent_management_group_id = "${var.root_id}"
+        subscription_ids           = []
+        archetype_config = {
+          archetype_id   = "default_empty"
+          parameters     = {}
+          access_control = {}
+        }
+      }
+      "${var.root_id}-landingzones" = {
+        display_name               = "Landing Zones"
+        parent_management_group_id = "${var.root_id}"
+        subscription_ids           = []
+        archetype_config = {
+          archetype_id   = "default_empty"
+          parameters     = {}
+          access_control = {}
+        }
+      }
+      "${var.root_id}-decom" = {
+        display_name               = "Decommissioned"
+        parent_management_group_id = "${var.root_id}"
+        subscription_ids           = []
+        archetype_config = {
+          archetype_id   = "default_empty"
+          parameters     = {}
+          access_control = {}
+        }
+        # depends_on= [azurerm_subscription.connectivity1,azurerm_subscription.connectivity2]
+      }
+      "${var.root_id}-sandbox" = {
+        display_name               = "Sandbox"
+        parent_management_group_id = "${var.root_id}"
+        subscription_ids           = []
+        archetype_config = {
+          archetype_id   = "default_empty"
+          parameters     = {}
+          access_control = {}
+        }
+      }
+      "${var.root_id}-iam" = {
+        display_name               = "Identity and Access Managment"
+        parent_management_group_id = "${var.root_id}-platform"
+        subscription_ids           = []
+        archetype_config = {
+          archetype_id   = "default_empty"
+          parameters     = {}
+          access_control = {}
+        }
+      }
+      ##### landing zone #####
+      "${var.root_id}-management" = {
+        display_name               = "Management"
+        parent_management_group_id = "${var.root_id}-platform"
+        subscription_ids           = []
+        archetype_config = {
+          archetype_id   = "default_empty"
+          parameters     = {}
+          access_control = {}
+        }
+      }
+      "${var.root_id}-connectivity" = {
+        display_name               = "Connectivity"
+        parent_management_group_id = "${var.root_id}-platform"
+        subscription_ids           = []
+        archetype_config = {
+          archetype_id   = "default_empty"
+          parameters     = {}
+          access_control = {}
+        }
+      }
+      "${var.root_id}-uat" = {
+        display_name               = "UAT"
+        parent_management_group_id = "${var.root_id}-landingzones"
+        subscription_ids           = []
+        archetype_config = {
+          archetype_id   = "default_empty"
+          parameters     = {}
+          access_control = {}
+        }
+      }
+      "${var.root_id}-dev" = {
+        display_name               = "Dev"
+        parent_management_group_id = "${var.root_id}-landingzones"
+        subscription_ids           = []
+        archetype_config = {
+          archetype_id   = "default_empty"
+          parameters     = {}
+          access_control = {}
+        }
+      }
+      "${var.root_id}-prod" = {
+        display_name               = "Prod"
+        parent_management_group_id = "${var.root_id}-landingzones"
+        subscription_ids           = []
+        archetype_config = {
+          archetype_id   = "default_empty"
+          parameters     = {}
+          access_control = {}
+        }
+      }
+  }
 }
