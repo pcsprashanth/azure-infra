b4d442d6877aff0c67673f865e9103c30631cda0
pcsprashanth <pathaneniprashanth@yahoo.in>
2024-04-17 19:35:20 +0530

    deleting management groups

--- main.tf
+++ main.tf
@@ -30,117 +30,117 @@ module "enterprise_scale" {
   deploy_core_landing_zones = false
   
 
-  custom_landing_zones = {
-      "${var.root_id}" = {
-        display_name               = "${lower(var.root_name)}"
-        parent_management_group_id = "${data.azurerm_client_config.core.tenant_id}"
-        subscription_ids           = []
-        archetype_config = {
-          archetype_id   = "es_root"
-          parameters     = {}
-          access_control = {}
-        }
-      }
-       "${var.root_id}-platform" = {
-        display_name               = "Platform"
-        parent_management_group_id = "${var.root_id}"
-        subscription_ids           = []
-        archetype_config = {
-          archetype_id   = "default_empty"
-          parameters     = {}
-          access_control = {}
-        }
-      }
-      "${var.root_id}-landingzones" = {
-        display_name               = "Landing Zones"
-        parent_management_group_id = "${var.root_id}"
-        subscription_ids           = []
-        archetype_config = {
-          archetype_id   = "default_empty"
-          parameters     = {}
-          access_control = {}
-        }
-      }
-      "${var.root_id}-sandbox" = {
-        display_name               = "Sandbox"
-        parent_management_group_id = "${var.root_id}"
-        subscription_ids           = []
-        archetype_config = {
-          archetype_id   = "default_empty"
-          parameters     = {}
-          access_control = {}
-        }
-      }
-      "${var.root_id}-admin" = {
-        display_name               = "admin"
-        parent_management_group_id = "${var.root_id}-platform"
-        subscription_ids           = []
-        archetype_config = {
-          archetype_id   = "default_empty"
-          parameters     = {}
-          access_control = {}
-        }
-      }
-      ##### landing zone #####
-      "${var.root_id}-common" = {
-        display_name               = "common"
-        parent_management_group_id = "${var.root_id}-platform"
-        subscription_ids           = []
-        archetype_config = {
-          archetype_id   = "default_empty"
-          parameters     = {}
-          access_control = {}
-        }
-      }
-      "${var.root_id}-security" = {
-        display_name               = "security"
-        parent_management_group_id = "${var.root_id}-platform"
-        subscription_ids           = []
-        archetype_config = {
-          archetype_id   = "default_empty"
-          parameters     = {}
-          access_control = {}
-        }
-      }
-      "${var.root_id}-foundation" = {
-        display_name               = "foundation"
-        parent_management_group_id = "${var.root_id}-platform"
-        subscription_ids           = []
-        archetype_config = {
-          archetype_id   = "default_empty"
-          parameters     = {}
-          access_control = {}
-        }
-      }
-      "${var.root_id}-cpcd" = {
-        display_name               = "cpcd"
-        parent_management_group_id = "${var.root_id}-landingzones"
-        subscription_ids           = []
-        archetype_config = {
-          archetype_id   = "default_empty"
-          parameters     = {}
-          access_control = {}
-        }
-      }
-      "${var.root_id}-pat" = {
-        display_name               = "pat"
-        parent_management_group_id = "${var.root_id}-landingzones"
-        subscription_ids           = []
-        archetype_config = {
-          archetype_id   = "default_empty"
-          parameters     = {}
-          access_control = {}
-        }
-      }
-      "${var.root_id}-dmz" = {
-        display_name               = "dmz"
-        parent_management_group_id = "${var.root_id}-landingzones"
-        subscription_ids           = []
-        archetype_config = {
-          archetype_id   = "default_empty"
-          parameters     = {}
-          access_control = {}
-        }
-      }
-  }
-}
+#   custom_landing_zones = {
+#       "${var.root_id}" = {
+#         display_name               = "${lower(var.root_name)}"
+#         parent_management_group_id = "${data.azurerm_client_config.core.tenant_id}"
+#         subscription_ids           = []
+#         archetype_config = {
+#           archetype_id   = "es_root"
+#           parameters     = {}
+#           access_control = {}
+#         }
+#       }
+#        "${var.root_id}-platform" = {
+#         display_name               = "Platform"
+#         parent_management_group_id = "${var.root_id}"
+#         subscription_ids           = []
+#         archetype_config = {
+#           archetype_id   = "default_empty"
+#           parameters     = {}
+#           access_control = {}
+#         }
+#       }
+#       "${var.root_id}-landingzones" = {
+#         display_name               = "Landing Zones"
+#         parent_management_group_id = "${var.root_id}"
+#         subscription_ids           = []
+#         archetype_config = {
+#           archetype_id   = "default_empty"
+#           parameters     = {}
+#           access_control = {}
+#         }
+#       }
+#       "${var.root_id}-sandbox" = {
+#         display_name               = "Sandbox"
+#         parent_management_group_id = "${var.root_id}"
+#         subscription_ids           = []
+#         archetype_config = {
+#           archetype_id   = "default_empty"
+#           parameters     = {}
+#           access_control = {}
+#         }
+#       }
+#       "${var.root_id}-admin" = {
+#         display_name               = "admin"
+#         parent_management_group_id = "${var.root_id}-platform"
+#         subscription_ids           = []
+#         archetype_config = {
+#           archetype_id   = "default_empty"
+#           parameters     = {}
+#           access_control = {}
+#         }
+#       }
+#       ##### landing zone #####
+#       "${var.root_id}-common" = {
+#         display_name               = "common"
+#         parent_management_group_id = "${var.root_id}-platform"
+#         subscription_ids           = []
+#         archetype_config = {
+#           archetype_id   = "default_empty"
+#           parameters     = {}
+#           access_control = {}
+#         }
+#       }
+#       "${var.root_id}-security" = {
+#         display_name               = "security"
+#         parent_management_group_id = "${var.root_id}-platform"
+#         subscription_ids           = []
+#         archetype_config = {
+#           archetype_id   = "default_empty"
+#           parameters     = {}
+#           access_control = {}
+#         }
+#       }
+#       "${var.root_id}-foundation" = {
+#         display_name               = "foundation"
+#         parent_management_group_id = "${var.root_id}-platform"
+#         subscription_ids           = []
+#         archetype_config = {
+#           archetype_id   = "default_empty"
+#           parameters     = {}
+#           access_control = {}
+#         }
+#       }
+#       "${var.root_id}-cpcd" = {
+#         display_name               = "cpcd"
+#         parent_management_group_id = "${var.root_id}-landingzones"
+#         subscription_ids           = []
+#         archetype_config = {
+#           archetype_id   = "default_empty"
+#           parameters     = {}
+#           access_control = {}
+#         }
+#       }
+#       "${var.root_id}-pat" = {
+#         display_name               = "pat"
+#         parent_management_group_id = "${var.root_id}-landingzones"
+#         subscription_ids           = []
+#         archetype_config = {
+#           archetype_id   = "default_empty"
+#           parameters     = {}
+#           access_control = {}
+#         }
+#       }
+#       "${var.root_id}-dmz" = {
+#         display_name               = "dmz"
+#         parent_management_group_id = "${var.root_id}-landingzones"
+#         subscription_ids           = []
+#         archetype_config = {
+#           archetype_id   = "default_empty"
+#           parameters     = {}
+#           access_control = {}
+#         }
+#       }
+#   }
+# }
